de9dcc83266180bc2f5e7b4856a2fcd07f1e4d6b
calebgucciardi <calebgucciardi@gmail.com>
2021-12-30 12:53:06 +0100

    created object track

--- db/app.py
+++ db/app.py
@@ -1,10 +1,10 @@
 import requests
-from requests.models import Response
+import pprint
 
 URL_GET_ARTIST_ALBUM = 'https://api.spotify.com/v1/artists/{}/albums?limit=5' # GET
 URL_GET_ALBUM = 'https://api.spotify.com/v1/albums/{}' # GET
 
-TOKEN = 'BQB0pvwifCNvPKusLtyov6yHCoGWpd_GZPCeGVFRVxKBOpXfLhs0FdjWkq62PjJBxt_xP-OBmGUYWAm1mesq7Yfbi4LHxYPgzN_g7otyGaze_e0TVMA066eZaOdYlJr0-u_EdeQxyBLbGPrm1kf8JaH8pZ9uIvg' 
+TOKEN = 'BQBjKA66O3lhytYqO2bycq-zi0A1h-sLadfZJh6nX6hVDYEW_gk2P7fk1WWy26BDWhXocCpKrWvSrTtG5l9x17oQW9B1ji_5ob8IZrasNEFBn_EXHTPYBhCCXSztS2IOliU7IiLLVdPXRhlm9Uf_D6q14YCCKIo' 
 
 
 def get_artist_album(id_artist):
@@ -26,16 +26,32 @@ def get_album(id_album):
             'Authorization' : 'Bearer {}'.format(TOKEN)
         }
     ).json()
-    obj = {}
+    
+    obj = {} # creazione oggetto album
     obj['id'] = id_album
     obj['title'] = response['name']
     obj['image'] = response['images'][0]['url']
     obj['date'] = response['release_date']
     obj['total_tracks'] = response['total_tracks']
-    obj['tracks'] = []
+    obj['duration'] = 0
+    
+    obj['tracks'] = [] # creazione lista di tracks dell'album
     for track in response['tracks']['items']:
-        obj['tracks'].append(track['id']) 
-    obj['artists'] = []
+        obj_track = {} # creazione oggetto track
+        obj_track['id'] = track['id']
+        obj_track['title'] = track['name']
+        obj_track['duration'] = track['duration_ms']
+        obj['duration'] += track['duration_ms'] # l'album ha come durata la somma delle durate delle tracks
+        obj_track['explicit'] = track['explicit']
+        obj_track['number'] = track['track_number']
+
+        obj_track['artists'] = []
+        for artist in track['artists']:
+            obj_track['artists'].append(artist['id']) 
+
+        obj['tracks'].append(obj_track) 
+
+    obj['artists'] = [] # creazione lista di artisti dell'album
     for artist in response['artists']:
         obj['artists'].append(artist['id'])
     return obj
@@ -43,7 +59,6 @@ def get_album(id_album):
 
 if __name__ == "__main__":
     album_list = get_artist_album('26T4yOaOoFJvUvxR87Y9HO') # id of 'bethel music' artist
-    for album in album_list:
-        print(album)
+    pprint.pprint(album_list[0])
 
 
