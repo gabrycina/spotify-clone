30d4e19bcda4f94380160d2c372ae8328be93f23
calebgucciardi <calebgucciardi@gmail.com>
2022-01-02 20:18:45 +0100

    added ft and making sql insert

--- db/app.py
+++ db/app.py
@@ -5,7 +5,7 @@ URL_GET_ARTIST_ALBUM = 'https://api.spotify.com/v1/artists/{}/albums?market=IT&l
 URL_GET_ALBUM = 'https://api.spotify.com/v1/albums/{}?market=IT' # GET
 URL_GET_TRACK_FEATURES = 'https://api.spotify.com/v1/audio-features/{}'
 
-TOKEN = 'BQDa0TaL57KfP2WPSrS9WzZfWPE6oVdG2-978teZcJ5bFwB5p2xWrPCLxpMJbW1tCNcq3OXE8rL0BCLWJ2fbwBPm9qovZvHP2gsr1GbXao3__1NAOMTX6ehwTEUloJi4l_gIfEl8RqCdPwi5t-u8o3NL7H6O4b0' 
+TOKEN = 'BQCYAd6Qiei9S2HuKEqp2lawWmNpTNX_ucTPSWW3ykfBKrEeoGUckRZDBmB6Q32LxtB15Eow-IKetv98vfrN9w9pc_1l_2SJWo4gU2x5sJRqrr11PMjvo8lGyjdIdeIzOzqAgTaj5ZjBdJ-s2LMaINIhyFvE5pk'
 
 
 def get_artist_album(id_artist):
@@ -67,46 +67,68 @@ def get_track_features(track):
             'Authorization' : 'Bearer {}'.format(TOKEN)
         }
     ).json() 
-    track['danceability'] = response['danceability']
-    track['energy'] = response['energy']
-    track['loudness'] = response['loudness']
-    track['speechiness'] = response['speechiness']
-    track['acousticness'] = response['acousticness']
-    track['instrumentalness'] = response['instrumentalness']
-    track['liveness'] = response['liveness']
-    track['valence'] = response['valence']
-    track['tempo'] = response['tempo']
+    track['danceability'] = round(response['danceability'],3)
+    track['energy'] = round(response['energy'],3)
+    track['loudness'] = round(response['loudness'],3)
+    track['speechiness'] = round(response['speechiness'],3)
+    track['acousticness'] = round(response['acousticness'],3)
+    track['instrumentalness'] = round(response['instrumentalness'],3)
+    track['liveness'] = round(response['liveness'],3)
+    track['valence'] = round(response['valence'],3)
+    track['tempo'] = round(response['tempo'],3)
     pass
 
 def obj_to_sql(album,set_artist):
-    insert_album = "INSERT INTO Album (id,title,numberOfTracks,image,durationMs,releaseDate)\nVALUES ('{}','{}',{},'{}',{},'{}');"
-    insert_track=  "VALUES ('{}','{}',{},{},{},{},{},{},{},{},{},{},{}){}"
-    # TrackBelongsToAlbum
-    insert_belong = "VALUES ({},{},{}){}"
+    insert_album = "INSERT INTO Album (id,title,numberOfTracks,image,durationMs,releaseDate)\nVALUES ('{}','{}',{},'{}',{},'{}');\n"
+    insert_track = ["INSERT INTO Track (id,title,danceability,energy,loudness,speechiness,acousticness,instrumentalness,liveness,valence,tempo,durationMs,isExplicit)\n",
+                    "VALUES ('{}','{}',{},{},{},{},{},{},{},{},{},{},{}){}\n"]
+    insert_belong = ["INSERT INTO TrackBelongsTo(album,track,position)\n",
+                     "VALUES ('{}','{}',{}){}\n"]
+    insert_making = ["INSERT INTO Making (artist,album)\n",
+                     "VALUES ('{}','{}'){}\n"]
+    insert_ft = "INSERT INTO Features (artist,track) VALUES ('{}','{}');\n"
 
     with open('prova.sql','a') as file:
 
         file.write("\n#--------------{}--------------\n".format(album['title']))
-        file.write(insert_album.format(album['id'],album['title'],album['total_tracks'],album['image'],album['duration'],album['date'])+'\n')
+        file.write(insert_album.format(album['id'],album['title'],album['total_tracks'],album['image'],album['duration'],album['date']))
         file.write('\n')
+
+        file.write(insert_making[0])
+        sep = ','
+        for n,maker in enumerate(album['artists']): # artisti in making
+            if n == len(album['artists'])-1:
+                sep = ';'
+            file.write(insert_making[1].format(maker,album['id'],sep))
+            set_artist.add(maker)
+        file.write('\n')
+
 
         track_list = []
         belong_list = []
+        ft_list = []
         sep = ','
-        for n,track in enumerate(album['tracks']):
-            if n== len(album['tracks'])-1:
+        for n,track in enumerate(album['tracks']): # track in album
+            if n == len(album['tracks'])-1:
                 sep = ';'
-            track_list.append(insert_track.format(track['id'],track['title'],track['danceability'],track['energy'],track['loudness'],track['speechiness'],track['acousticness'],track['instrumentalness'],track['liveness'],track['valence'],track['tempo'],track['duration'],track['explicit'],sep)+'\n')
-            belong_list.append(insert_belong.format(album['id'],track['id'],n,sep)+'\n')
+            track_list.append(insert_track[1].format(track['id'],track['title'],track['danceability'],track['energy'],track['loudness'],track['speechiness'],track['acousticness'],track['instrumentalness'],track['liveness'],track['valence'],track['tempo'],track['duration'],track['explicit'],sep))
+            belong_list.append(insert_belong[1].format(album['id'],track['id'],n,sep))
+            for ft in track['ft']: # ft in track
+                ft_list.append(insert_ft.format(ft,track['id']))
+                set_artist.add(ft)
+
         
-        file.write("INSERT INTO Track (id,title,danceability,energy,loudness,speechiness,acousticness,instrumentalness,liveness,valence,tempo,durationMs,isExplicit)\n")
+        file.write(insert_track[0])
         file.writelines(track_list)
         file.write('\n')
 
-        file.write("INSERT INTO TrackBelongsTo(album,track,number)\n")
+        file.write(insert_belong[0])
         file.writelines(belong_list)
-        file.write('#----------------------------------------------\n\n')
+        file.write('\n')
 
+        if len(ft_list) != 0:
+            file.writelines(ft_list)
+        file.write('#----------------------------------------------\n\n')
     pass
 
 if __name__ == "__main__":
