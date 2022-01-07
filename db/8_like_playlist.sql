ac1e18561039da6eca338adeea484ec9a38d211d
Francesco <francesco0501@libero.it>
2022-01-07 19:25:00 +0100

    added likePlaylist

---
+++ db/8_like_playlist.sql
@@ -0,0 +1,29 @@
+#-------likesPlaylist Emma rock------
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('WSVaeQF40xY6qEhhSCsbfA','QKedDkxLZFOPQS8pAhkj01','2021-03-12');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('mOd5giEfK8mWOCgB2QTM8s','QKedDkxLZFOPQS8pAhkj01','2021-06-21');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('R1kn0yocXXUlgLKG79yWH7','QKedDkxLZFOPQS8pAhkj01','2021-09-08');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('AGo63tostpFODB023uapny','QKedDkxLZFOPQS8pAhkj01','2021-12-31');
+
+#-------likesPlaylist Andres rap------
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('PrsS4E1r054PsZUyl4MTix','fDvg6G63EGWEDstKuQ3u02','2021-03-12');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('JbgJWXvmkciLFI3mQpGYjL','fDvg6G63EGWEDstKuQ3u02','2021-06-21');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('8rNOd494DEYPasXarQUx5t','fDvg6G63EGWEDstKuQ3u02','2021-09-08');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('fY1hQJ08pO7QHlPcB3uIvG','fDvg6G63EGWEDstKuQ3u02','2021-12-31');
+
+#-------likesPlaylist Gabby country------
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('GB7mqCD8Qjw2gHRg1854Qm','9fPfGMn2IJJZG1Sy0J1t03','2021-03-12');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('7Tu7fhjaFbJabiZTj6i30x','9fPfGMn2IJJZG1Sy0J1t03','2021-06-21');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('6ijXKzWrvbH9AfBTP6A6EJ','9fPfGMn2IJJZG1Sy0J1t03','2021-09-08');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('2Fk174hLD7WUFNrC5Ct54n','9fPfGMn2IJJZG1Sy0J1t03','2021-12-31');
+
+#-------likesPlaylist Ryoga pop------
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('uEW6GeHCTKiLAkrB2A3cOZ','wz9xHeLMgQpjo7Nhwidp04','2021-03-12');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('pUU47F2AVbrbrZn09ZvlnZ','wz9xHeLMgQpjo7Nhwidp04','2021-06-21');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('VFq9fMG3WU07YmAPVOuRin','wz9xHeLMgQpjo7Nhwidp04','2021-09-08');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('2Fk174hLD7WUFNrC5Ct54n','wz9xHeLMgQpjo7Nhwidp04','2021-12-31');
+
+#-------likesPlaylist Luke hip hop------
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('NA2aYqVdEALi499lRYuqfu','RBtBUZTItc3plsuGbxGJ05','2021-03-12');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('eZnRqL7nh7NXW2EOQ2B8kd','RBtBUZTItc3plsuGbxGJ05','2021-06-21');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('kb2BooEX18RNOOWIoo3bUO','RBtBUZTItc3plsuGbxGJ05','2021-09-08');
+INSERT INTO LikesPlaylist(playlist,user,date) VALUES('FjjwVrno0mJ57HybmyYSJ8','RBtBUZTItc3plsuGbxGJ05','2021-12-31');
