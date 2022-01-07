59bb0154448105a6993541bb7f4dc140bfb34618
Matteo-unroot <librizzimatteo.ml@gmail.com>
2022-01-07 14:43:52 +0100

    slight edit in user

--- db/user (5).sql
+++ db/user (5).sql
@@ -1,5 +1,5 @@
-INSERT INTO User(id,username,email,password) VALUES(0,"Emma rock","Emma@gmail.com","securePassword553382550");
-INSERT INTO User(id,username,email,password) VALUES(1,"Andres rap","Andres@gmail.com","securePassword-382276507");
-INSERT INTO User(id,username,email,password) VALUES(2,"Gabby country","Gabby@gmail.com","securePassword-1789493239");
-INSERT INTO User(id,username,email,password) VALUES(3,"Ryoga pop","Ryoga@gmail.com","securePassword717215266");
-INSERT INTO User(id,username,email,password) VALUES(4,"Luke hip hop","Luke@gmail.com","securePassword-328947188");
+INSERT INTO User(id,username,email,password) VALUES('0',"Emma rock","Emma@gmail.com","securePassword553382550");
+INSERT INTO User(id,username,email,password) VALUES('1',"Andres rap","Andres@gmail.com","securePassword-382276507");
+INSERT INTO User(id,username,email,password) VALUES('2',"Gabby country","Gabby@gmail.com","securePassword-1789493239");
+INSERT INTO User(id,username,email,password) VALUES('3',"Ryoga pop","Ryoga@gmail.com","securePassword717215266");
+INSERT INTO User(id,username,email,password) VALUES('4',"Luke hip hop","Luke@gmail.com","securePassword-328947188");
