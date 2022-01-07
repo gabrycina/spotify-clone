65abf409ade9c4cfc55cf81d3ecea0f9207d1633
calebgucciardi <calebgucciardi@gmail.com>
2022-01-07 16:19:35 +0100

    added this is pop playlist

--- db/spotty (4).sql
+++ db/spotty (4).sql
@@ -1,8 +1,9 @@
 # la password è 'spotty'
 INSERT INTO User (id,username,email,password,dob) 
 VALUES ('spotty#id#123456789123','Spotty','spotty@spotty.com','010f167a05df2bb89c4c39023e57dab1f411bd2f087c36a40b5779949aba6f44','1900-01-01');
+# ------------------------------------
 
-# Playlist di Sistema 'This is Gospel'
+# ----------- Playlist di Sistema 'This is Gospel'-------------
 INSERT INTO Playlist (id,creator,name,description)
 VALUES ('2jU0k1emxxycV673rppvzT','spotty#id#123456789123','This is Gospel','Top 20 Gosepl Songs');
 
@@ -27,3 +28,31 @@ INSERT INTO TrackBelongsToPlaylist(track,playlist,addedDate) VALUES
 ('2iBDNtyifH3CtG7Y5VDeAz','2jU0k1emxxycV673rppvzT','2022-01-06'),
 ('2nRlozYqGWVMQhFoi5grjD','2jU0k1emxxycV673rppvzT','2022-01-06'),
 ('2qry1lxjBAboHdzWIbBkwG','2jU0k1emxxycV673rppvzT','2022-01-06');
+# --------------------------------------------------------------
+
+# ------------ Playlist di Sistema 'This is Pop'----------------
+INSERT INTO Playlist (id,creator,name,description)
+VALUES ('bucBJjTnucfsqIQHoQB3KE','spotty#id#123456789123','This is Pop','Top 20 Pop Songs');
+
+INSERT INTO TrackBelongsToPlaylist(track,playlist,addedDate) VALUES
+('03blI4F6MeYd6kJx26VsJ2','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('07Wqp9LcTwzdIyuI0twXjx','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('08aMpsxjozNNQPVnnE1QaC','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('09u787BYeYIGd2mFIJ505t','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0aeqToKAIt951omSGAyLS4','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0bqC0AqaBZKBZsjhI3y2OW','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0ct6r3EGTcMLPtrXHDvVjc','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0DuWDLjriRPjDRoPgaCslY','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0GxQ1A5L9xnMOytbP6eKBG','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0HZgYFimoJG9Ijy8InUWcV','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0JZ9TvOLtZJaGqIyC4hYZX','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0oaY19dUwZimIgzn3ZZLZO','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0oOOVSZGuPriWBqwbh0uuJ','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0RqTAf0gX3pXAJYAbYRDqA','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0vrmHPfoBadXVr2n0m1aqZ','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('0xeYgM5UfwXZ1JEdT2MkhQ','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('10ImcQk9tihY1EKMDIbvXJ','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('137mSruegm1u6x9NvwiagR','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('13CVSGLSFl4UxpDVR6u3dq','bucBJjTnucfsqIQHoQB3KE','2022-01-07'),
+('1CRtJS94Hq3PbBZT9LuF90','bucBJjTnucfsqIQHoQB3KE','2022-01-07');
+# --------------------------------------------------------------
