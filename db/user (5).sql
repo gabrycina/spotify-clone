7dc77726c7b69a2e7f23190c19ac32bfe6109393
Francesco <francesco0501@libero.it>
2022-01-07 16:22:24 +0100

    fixed char(22) for id

--- db/user (5).sql
+++ db/user (5).sql
@@ -1,5 +1,5 @@
-INSERT INTO User(id,username,email,password) VALUES('0',"Emma rock","Emma@gmail.com","securePassword553382550");
-INSERT INTO User(id,username,email,password) VALUES('1',"Andres rap","Andres@gmail.com","securePassword-382276507");
-INSERT INTO User(id,username,email,password) VALUES('2',"Gabby country","Gabby@gmail.com","securePassword-1789493239");
-INSERT INTO User(id,username,email,password) VALUES('3',"Ryoga pop","Ryoga@gmail.com","securePassword717215266");
-INSERT INTO User(id,username,email,password) VALUES('4',"Luke hip hop","Luke@gmail.com","securePassword-328947188");
+INSERT INTO User(id,username,email,password) VALUES('QKedDkxLZFOPQS8pAhkj01','Emma rock','Emma@gmail.com','securePassword553382550');
+INSERT INTO User(id,username,email,password) VALUES('fDvg6G63EGWEDstKuQ3u02','Andres rap','Andres@gmail.com','securePassword382276507');
+INSERT INTO User(id,username,email,password) VALUES('9fPfGMn2IJJZG1Sy0J1t03','Gabby country','Gabby@gmail.com','securePassword1789493239');
+INSERT INTO User(id,username,email,password) VALUES('wz9xHeLMgQpjo7Nhwidp04','Ryoga pop','Ryoga@gmail.com','securePassword717215266');
+INSERT INTO User(id,username,email,password) VALUES('RBtBUZTItc3plsuGbxGJ05','Luke hip hop','Luke@gmail.com','securePassword328947188');
