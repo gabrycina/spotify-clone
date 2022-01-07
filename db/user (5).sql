e92e1b8479b35e495f19c615708ac5819b226f48
calebgucciardi <calebgucciardi@gmail.com>
2022-01-07 17:07:25 +0100

    added dailysuggestion for users + changed password to users

--- db/user (5).sql
+++ db/user (5).sql
@@ -1,5 +1,50 @@
-INSERT INTO User(id,username,email,password) VALUES('QKedDkxLZFOPQS8pAhkj01','Emma rock','Emma@gmail.com','securePassword553382550');
-INSERT INTO User(id,username,email,password) VALUES('fDvg6G63EGWEDstKuQ3u02','Andres rap','Andres@gmail.com','securePassword382276507');
-INSERT INTO User(id,username,email,password) VALUES('9fPfGMn2IJJZG1Sy0J1t03','Gabby country','Gabby@gmail.com','securePassword1789493239');
-INSERT INTO User(id,username,email,password) VALUES('wz9xHeLMgQpjo7Nhwidp04','Ryoga pop','Ryoga@gmail.com','securePassword717215266');
-INSERT INTO User(id,username,email,password) VALUES('RBtBUZTItc3plsuGbxGJ05','Luke hip hop','Luke@gmail.com','securePassword328947188');
+#---------------- password emma ---------------------------
+INSERT INTO User(id,username,email,password) VALUES('QKedDkxLZFOPQS8pAhkj01','Emma rock','Emma@gmail.com','f4aa0655cdb8d4fcf6f719c7a786de10556783c70bfb8ef1d78923482fe6ebbc');
+
+INSERT INTO Playlist (id,creator,name,isDailySuggestion)
+VALUES ('qkEDdKXlzfopqs8PaHKJ01','spotty#id#123456789123','DailyMix',1);
+
+INSERT INTO DailySuggestion (id,suggestedFor)
+VALUES ('qkEDdKXlzfopqs8PaHKJ01','QKedDkxLZFOPQS8pAhkj01');
+#---------------------------------------------------------
+
+#------------- password andres ---------------------------
+INSERT INTO User(id,username,email,password) VALUES('fDvg6G63EGWEDstKuQ3u02','Andres rap','Andres@gmail.com','0a92efb1b91ac02c858ab205fbb6baf44d67e8d1e625600a11020cfae50065da');
+
+INSERT INTO Playlist (id,creator,name,isDailySuggestion)
+VALUES ('FdVG6g63egwedSTkUq3U02','spotty#id#123456789123','DailyMix',1);
+
+INSERT INTO DailySuggestion (id,suggestedFor)
+VALUES ('FdVG6g63egwedSTkUq3U02','fDvg6G63EGWEDstKuQ3u02');
+#----------------------------------------------------------
+
+#------------------- password gabby -----------------------
+INSERT INTO User(id,username,email,password) VALUES('9fPfGMn2IJJZG1Sy0J1t03','Gabby country','Gabby@gmail.com','c7b9bdd40758eaa827b7abc1fc31408901ac99cef4f3891c1fb3598f531bb9e6');
+
+INSERT INTO Playlist (id,creator,name,isDailySuggestion)
+VALUES ('9FpFgmN2ijjzg1sY0j1T03','spotty#id#123456789123','DailyMix',1);
+
+INSERT INTO DailySuggestion (id,suggestedFor)
+VALUES ('9FpFgmN2ijjzg1sY0j1T03','9fPfGMn2IJJZG1Sy0J1t03');
+#----------------------------------------------------------
+
+#------------------- password ryoga -----------------------
+INSERT INTO User(id,username,email,password) VALUES('wz9xHeLMgQpjo7Nhwidp04','Ryoga pop','Ryoga@gmail.com','cedee8097b415089c67aff92192cd32ef2ffb7ea86fcbc0900ec071362c07994');
+
+INSERT INTO Playlist (id,creator,name,isDailySuggestion)
+VALUES ('WZ9XhElmGqPJO7nHWIDP04','spotty#id#123456789123','DailyMix',1);
+
+INSERT INTO DailySuggestion (id,suggestedFor)
+VALUES ('WZ9XhElmGqPJO7nHWIDP04','wz9xHeLMgQpjo7Nhwidp04');
+#----------------------------------------------------------
+
+
+#-- password luke 
+INSERT INTO User(id,username,email,password) VALUES('RBtBUZTItc3plsuGbxGJ05','Luke hip hop','Luke@gmail.com','cccf52f3d03c5072ee64a136f74fc56b27e04f15ffe5d1d277d58ab18d94b327');
+
+INSERT INTO Playlist (id,creator,name,isDailySuggestion)
+VALUES ('rbTbuztiTC3PLSUgBXgj05','spotty#id#123456789123','DailyMix',1);
+
+INSERT INTO DailySuggestion (id,suggestedFor)
+VALUES ('rbTbuztiTC3PLSUgBXgj05','RBtBUZTItc3plsuGbxGJ05');
+#----------------------------------------------------------
