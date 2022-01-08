274cc627841e3861dc45a9220103af3df33d2dd5
calebgucciardi <calebgucciardi@gmail.com>
2022-01-08 13:55:22 +0100

    added query for ArtistBelongsToBand

---
+++ db/13_artist_belongs_to_band.sql
@@ -0,0 +1,13 @@
+#-------------------- Bethel Artist ------------------------
+INSERT INTO ArtistBelongsToBand (singleArtist,band,role) VALUES
+('53Gnd3lGlcL8ua9Yyu9xDP','26T4yOaOoFJvUvxR87Y9HO','singer'),
+('2gXmjQIxCO8lMnSncluZaU','26T4yOaOoFJvUvxR87Y9HO','singer'),
+('16f4gB92qCHbrYWixnzDGo','26T4yOaOoFJvUvxR87Y9HO','singer');
+#------------------------------------------------------------
+
+#--------------------- Israel & New Breed -------------------
+INSERT INTO ArtistBelongsToBand (singleArtist,band,role) VALUES
+('47opbYEKDjXnRk9uLscp11','77HU1Zb1VDIFvWKteJii0E','singer'),
+('3v2b4owGaN9d2bSdsZMWxR','77HU1Zb1VDIFvWKteJii0E','singer'),
+('758REhgt1yL3yL6NhwRqTt','77HU1Zb1VDIFvWKteJii0E','singer');
+#------------------------------------------------------------
