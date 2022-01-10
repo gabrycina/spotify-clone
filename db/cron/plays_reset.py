129e8612bc3b1b5992612db9ccfc2f57d38e751e
gabrycina <c.gabriele.info@gmail.com>
2022-01-10 16:51:56 +0100

    Added plays_reset script.py final

--- db/cron/plays_reset.py
+++ db/cron/plays_reset.py
@@ -1,29 +1,29 @@
 import mysql.connector as db
 
 # DB CONNECTION
-connection = db.connect(user='root', password='admin', host='localhost',
+connection = db.connect(user='root', password='', host='localhost',
                         database='spotty', auth_plugin='mysql_native_password')
 cursor = connection.cursor()
 
 # QUERY
 GET_TRACKS_QUERY = (
-    "select T.id"
-    "from Track T;"
+    "select T.id "
+    "from Track T"
 )
 
 UPDATE_TRACK = (
-    "update Track"
-    "set plays=0"
+    "update Track "
+    "set plays=0 "
     "where id='{}'"
 )
 
 # GETTING ALL TRACKS
 cursor.execute(GET_TRACKS_QUERY)
-tracks = cursor.fetchall() 
+tracks = cursor.fetchall()
 
 for track in tracks:
     # reset plays
     cursor.execute(UPDATE_TRACK.format(track[0]))
- 
+
 connection.commit()
 connection.close()
