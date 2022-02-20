d4fb37e566f7c0f75c470e6003d3e78952c9f586
calebgucciardi <calebgucciardi@gmail.com>
2022-02-20 20:24:16 +0100

    listen/track endpoint

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from search import search_bp
 from auth import auth_bp
 from welcome import welcome_bp
 from like import like_bp
+from listen import listen_bp
 
 app.register_blueprint(preview_bp, url_prefix='/preview')
 app.register_blueprint(suggester_bp, url_prefix='/suggester')
@@ -12,5 +13,6 @@ app.register_blueprint(search_bp, url_prefix='/search')
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(like_bp, url_prefix='/like')
 app.register_blueprint(welcome_bp, url_prefix='/welcome')
+app.register_blueprint(listen_bp,url_prefix='/listen')
 
 app.run()
---
+++ backend/listen.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, request, jsonify
+from __init__ import mysql
+import MySQLdb.cursors
+
+listen_bp = Blueprint('listen', __name__)
+
+@listen_bp.route('/track/',methods=['POST'])
+def listen():
+    content = request.json
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(f"insert into ListenedTo (user,track,date) values ('{content['user']}','{content['id']}',NOW())")
+    mysql.connection.commit()
+    return jsonify({"msg":"done"})
