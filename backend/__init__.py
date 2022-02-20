51a61503df168e8d26cd6b20ad18d82672e99007
gabrycina <c.gabriele.info@gmail.com>
2022-02-20 18:19:35 +0100

    Added requirements.txt

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,11 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'spotty'
 
-mysql = MySQL(app) 
+mysql = MySQL(app)
