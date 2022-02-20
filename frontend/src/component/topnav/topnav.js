84dfe556d913c4173ceeffeb9a8f34537cd9e1af
gabrycina <c.gabriele.info@gmail.com>
2022-02-21 00:37:38 +0100

    Solved sign out bug redirect

--- frontend/src/component/topnav/topnav.js
+++ frontend/src/component/topnav/topnav.js
@@ -4,19 +4,21 @@ import SearchBox from "./search-box";
 import LibraryTabBtn from "./library-tab-btn";
 import { ReactReduxContext, useDispatch } from "react-redux";
 import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import styles from "./topnav.module.css";
 import { logout } from "../../actions";
 
 function Topnav({ search = false, tabButtons = false }) {
   const { store } = useContext(ReactReduxContext);
-
+  let history = useHistory();
   const dispatch = useDispatch();
 
   const handleLogout = (e) => {
     e.preventDefault();
 
     dispatch(logout());
+    history.push("/");
   };
 
   return (
