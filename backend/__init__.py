f9e2bd3529c909d475fe89dccb0c690aafc543b8
gabrycina <c.gabriele.info@gmail.com>
2022-02-21 00:58:54 +0100

    Added "no results found" page in search

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,7 +7,7 @@ CORS(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'rootroot'
+app.config['MYSQL_PASSWORD'] = 'Polpopolpo'
 app.config['MYSQL_DB'] = 'spotty'
 
 mysql = MySQL(app)
--- frontend/src/component/results/results.js
+++ frontend/src/component/results/results.js
@@ -5,43 +5,60 @@ import PlaylistCardM from "../cards/playlist-card-m";
 import TrackCard from "../cards/track-card";
 
 function Results(props) {
-  return (
-    <div className={styles.Content}>
-      <section>
-        <div className={styles.SectionTitle}>
-          <TitleL>Tracks</TitleL>
-        </div>
+  if (
+    props.data.tracks?.length == 0 &&
+    props.data.albums?.length == 0 &&
+    props.data.playlists?.length == 0
+  ) {
+    return (
+      <div className="opacity-40 text-center my-80 text-4xl">
+        <h1>We did't find music with this search...</h1>
+        <h1>Why don't you try with something different?</h1>
+        <br />
+        <span role="img" aria-label="headphones" aria-labelledby="hp">
+          🎧
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.Content}>
+        <section>
+          <div className={styles.SectionTitle}>
+            <TitleL>Tracks</TitleL>
+          </div>
 
-        <div className={styles.SectionCardsMedium}>
-          {props.data.tracks?.map((item) => {
-            return <TrackCard key={item.title} data={item} />;
-          })}
-        </div>
-      </section>
-      <section>
-        <div className={styles.SectionTitle}>
-          <TitleL>Albums</TitleL>
-        </div>
+          <div className={styles.SectionCardsMedium}>
+            {props.data.tracks?.map((item) => {
+              return <TrackCard key={item.title} data={item} />;
+            })}
+          </div>
+        </section>
+        <section>
+          <div className={styles.SectionTitle}>
+            <TitleL>Albums</TitleL>
+          </div>
 
-        <div className={styles.SectionCardsMedium}>
-          {props.data.albums?.map((item) => {
-            return <PlaylistCardM key={item.title} data={item} />;
-          })}
-        </div>
-      </section>
-      <section className="mb-60">
-        <div className={styles.SectionTitle}>
-          <TitleL>Playlists</TitleL>
-        </div>
+          <div className={styles.SectionCardsMedium}>
+            {props.data.albums?.map((item) => {
+              return <PlaylistCardM key={item.title} data={item} />;
+            })}
+          </div>
+        </section>
+        <section className="mb-60">
+          <div className={styles.SectionTitle}>
+            <TitleL>Playlists</TitleL>
+          </div>
 
-        <div className={styles.SectionCardsMedium}>
-          {props.data.playlists?.map((item) => {
-            return <PlaylistCardM key={item.title} data={item} />;
-          })}
-        </div>
-      </section>
-    </div>
-  );
+          <div className={styles.SectionCardsMedium}>
+            {props.data.playlists?.map((item) => {
+              return <PlaylistCardM key={item.title} data={item} />;
+            })}
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Results;
