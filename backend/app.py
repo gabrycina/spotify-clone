5031473c84b1e2a7e5809651f4cc253a3e3811a3
calebgucciardi <calebgucciardi@gmail.com>
2022-02-20 09:40:30 +0100

    Start flak project

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'spotty'
+
+mysql = MySQL(app)
+
+
+if __name__ == '__main__':
+    app.run()
