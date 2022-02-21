1bcb04653f5c60cb2e49204fccc2f0993aa59211
gabrycina <c.gabriele.info@gmail.com>
2022-02-21 20:12:12 +0100

    Library connected to backend

---
+++ frontend/src/component/cards/artist-card.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import TextBoldL from "../text/text-bold-l";
+import styles from "./artist-card.module.css";
+import TextRegularM from "../text/text-regular-m";
+
+function ArtistCard(props) {
+  return (
+    <div className={styles.ArtistCardBox}>
+      <Link to={`/artist/${props.data.id}`}>
+        <div className={styles.ArtistCard}>
+          <div className={styles.ImgBox}>
+            <img
+              src={props.data.artistimg}
+              alt={props.data.name}
+              style={{
+                borderRadius: "50%",
+                background: "red",
+                display: "block",
+              }}
+            />
+          </div>
+          <div className={styles.Title}>
+            <TextBoldL>{props.data.name}</TextBoldL>
+            <TextRegularM>Artist</TextRegularM>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default ArtistCard;
--- frontend/src/pages/library.js
+++ frontend/src/pages/library.js
@@ -3,8 +3,11 @@ import TitleM from "../component/text/title-m";
 import Topnav from "../component/topnav/topnav";
 import PlaylistCardM from "../component/cards/playlist-card-m";
 import { PLAYLIST } from "../data/index";
-
+import { selectUser } from "../reducers/index";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import styles from "./library.module.css";
+import ArtistCard from "../component/cards/artist-card";
 
 function Library() {
   return (
@@ -26,38 +29,143 @@ function Library() {
 }
 
 function PlaylistTab() {
-  return (
+  const [playlists, setplaylists] = useState([]);
+  const user = useSelector(selectUser);
+  const [isLoading, setisLoading] = useState(true);
+
+  const updatePlaylists = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: user.id, type: "playlist" }),
+      mode: "cors",
+    };
+
+    const fetchedPlaylists = await fetch(
+      "http://127.0.0.1:5000/library/get/",
+      requestOptions
+    ).then((fetchedPlaylists) => fetchedPlaylists.json());
+
+    setplaylists(fetchedPlaylists);
+    setisLoading(false);
+  };
+
+  useEffect(() => {
+    updatePlaylists();
+  }, [user]);
+
+  return isLoading ? (
+    <div className={styles.Content}>
+      <div className="flex justify-center items-center mt-80">
+        <div className="loader p-5 rounded-full flex space-x-3">
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+        </div>
+      </div>
+    </div>
+  ) : (
     <div>
       <TitleM>Playlists</TitleM>
       <div className={styles.Grid}>
-        {/* 
-        //todo: use api
-        {PLAYLIST.filter((item) => item.type == "playlist").map((item) => {
+        {playlists.map((item) => {
           return <PlaylistCardM key={item.title} data={item} />;
-        })} */}
+        })}
       </div>
     </div>
   );
 }
 
 function ArtistTab() {
-  return (
+  const [artists, setArtists] = useState([]);
+  const user = useSelector(selectUser);
+  const [isLoading, setisLoading] = useState(true);
+
+  const updateArtists = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: user.id, type: "artists" }),
+      mode: "cors",
+    };
+
+    const fetchedArtists = await fetch(
+      "http://127.0.0.1:5000/library/get/",
+      requestOptions
+    ).then((fetchedArtists) => fetchedArtists.json());
+
+    setArtists(fetchedArtists);
+    setisLoading(false);
+  };
+
+  useEffect(() => {
+    updateArtists();
+  }, [user]);
+  return isLoading ? (
+    <div className={styles.Content}>
+      <div className="flex justify-center items-center mt-80">
+        <div className="loader p-5 rounded-full flex space-x-3">
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+        </div>
+      </div>
+    </div>
+  ) : (
     <div>
       <TitleM>Artists</TitleM>
+      <div className={styles.Grid}>
+        {artists.map((item) => {
+          return <ArtistCard key={item.name} data={item} />;
+        })}
+      </div>
     </div>
   );
 }
 
 function AlbumTab() {
-  return (
+  const [albums, setAlbums] = useState([]);
+  const user = useSelector(selectUser);
+  const [isLoading, setisLoading] = useState(true);
+
+  const updateAlbums = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: user.id, type: "album" }),
+      mode: "cors",
+    };
+
+    const fetchedAlbums = await fetch(
+      "http://127.0.0.1:5000/library/get/",
+      requestOptions
+    ).then((fetchedAlbums) => fetchedAlbums.json());
+
+    setAlbums(fetchedAlbums);
+    setisLoading(false);
+  };
+
+  useEffect(() => {
+    updateAlbums();
+  }, [user]);
+
+  return isLoading ? (
+    <div className={styles.Content}>
+      <div className="flex justify-center items-center mt-80">
+        <div className="loader p-5 rounded-full flex space-x-3">
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+          <div className="w-3 h-3 bg-white rounded-full animate-bounce"></div>
+        </div>
+      </div>
+    </div>
+  ) : (
     <div>
       <TitleM>Albums</TitleM>
       <div className={styles.Grid}>
-        {/* 
-        //todo: use api
-        {PLAYLIST.filter((item) => item.type == "albüm").map((item) => {
+        {albums.map((item) => {
           return <PlaylistCardM key={item.title} data={item} />;
-        })} */}
+        })}
       </div>
     </div>
   );
